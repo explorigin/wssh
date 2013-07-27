1be1b1671fe15234be36e717b806208c39852840
Timothy Farrell <explorigin@gmail.com>
2013-07-27 15:47:22 +0000

    Cleanup resizing a little better and remove private key auth for now (the idea is of transmitting a private key of TLS is risky at best).

--- wssh/static/viewmodel.js
+++ wssh/static/viewmodel.js
@@ -3,8 +3,7 @@
 
     function TerminalSize() {
         var self = this,
-            terminals = [],
-            termEl, el, sizeCallback;
+            terminals = [];
 
         this.characterBox = {
             width: 0,
@@ -58,8 +57,10 @@
         };
 
         // Determine character width
-        function step1() {
-            termEl = $('<div class="terminal"></div>').appendTo(document.body),
+        this.determineCharacterWidth = function(callback) {
+            var termEl, el;
+
+            termEl =  $('<div class="terminal"></div>').appendTo(document.body),
             el = $('<div style="visibility: hidden; display:inline-block">M</div>').appendTo(termEl),
 
             self.characterBox.width = el.innerWidth();
@@ -68,24 +69,25 @@
             el.text("MM");
 
             setTimeout(step2, 0);
-        }
 
-        function step2() {
-            self.characterBox.width = el.innerWidth() - self.characterBox.width;
+            function step2() {
+                self.characterBox.width = el.innerWidth() - self.characterBox.width;
 
-            termEl.remove();
+                termEl.remove();
+                termEl = null;
+                el = null;
 
-            if (sizeCallback) {
-                sizeCallback();
+                if (callback !== undefined) {
+                    callback();
+                }
             }
         }
 
         $(window).resize(function() {
-            sizeCallback = self.resizeAllTerminals;
-            step1();
+            self.determineCharacterWidth(self.resizeAllTerminals)
         });
 
-        setTimeout(step1, 0);
+        setTimeout(this.determineCharacterWidth, 0);
     }
 
     tSize = new TerminalSize();
@@ -125,21 +127,21 @@
 
     $(function() {
         $('#ssh').hide();
-        $('#private_key_authentication', '#connect').hide();
+        // $('#private_key_authentication', '#connect').hide();
 
-        $('input:radio[value=private_key]', '#connect').click(
-            function() {
-                $('#password_authentication').hide();
-                $('#private_key_authentication').show();
-            }
-        );
+        // $('input:radio[value=private_key]', '#connect').click(
+        //     function() {
+        //         $('#password_authentication').hide();
+        //         $('#private_key_authentication').show();
+        //     }
+        // );
 
-        $('input:radio[value=password]', '#connect').click(
-            function() {
-                $('#password_authentication').show();
-                $('#private_key_authentication').hide();
-            }
-        );
+        // $('input:radio[value=password]', '#connect').click(
+        //     function() {
+        //         $('#password_authentication').show();
+        //         $('#private_key_authentication').hide();
+        //     }
+        // );
 
         $('#connect').submit(function(ev) {
             ev.preventDefault();
@@ -164,32 +166,33 @@
 
             $('.navbar .brand').text(username.val() + '@' + hostname.val());
 
-            var authentication = $(
-                'input[name=authentication_method]:checked',
-                '#connect').val();
+            // var authentication = $(
+            //     'input[name=authentication_method]:checked',
+            //     '#connect').val();
 
             var options = {
                 username: username.val(),
                 hostname: hostname.val(),
-                authentication_method: authentication
+                authentication_method: 'password'
+                // authentication_method: authentication
             };
 
-            if (authentication == 'password') {
+            // if (authentication == 'password') {
                 var password = $('input:password#password');
                 if (!validate([username, hostname, password]))
                     return false;
                 $.extend(options, {password: password.val()});
-            } else if (authentication == 'private_key') {
-                var private_key = $('textarea#private_key');
-                if (!validate([username, hostname, private_key]))
-                    return false;
-                $.extend(options, {private_key: private_key.val()});
-                var key_passphrase = $('input:password#key_passphrase');
-                if (key_passphrase.val()) {
-                    $.extend(options,
-                        {key_passphrase: key_passphrase.val()});
-                }
-            }
+            // } else if (authentication == 'private_key') {
+            //     var private_key = $('textarea#private_key');
+            //     if (!validate([username, hostname, private_key]))
+            //         return false;
+            //     $.extend(options, {private_key: private_key.val()});
+            //     var key_passphrase = $('input:password#key_passphrase');
+            //     if (key_passphrase.val()) {
+            //         $.extend(options,
+            //             {key_passphrase: key_passphrase.val()});
+            //     }
+            // }
 
             $('#connect').hide();
             $('#ssh').show();
